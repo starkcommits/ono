ee63f83ffdb0560c52040ea45df23d43039313c9
Stark-commits <tinkal@india100x.com>
2025-03-28 06:54:55 +0000

    updated market socket

--- rewardapp/hooks.py
+++ rewardapp/hooks.py
@@ -168,9 +168,11 @@ api = {
 }
 
 scheduler_events = {
-    "*/1 * * * *": [
-        "rewardapp.tasks.execute"
-    ]
+    "cron": {
+        "*/1 * * * *": [
+            "rewardapp.tasks.execute"
+        ]
+    }
 }
 
 # Scheduled Tasks
