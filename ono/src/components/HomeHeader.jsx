a3d61bac42972e1b4c6e3e872d11b3952836a1c6
mitsuxhoney <hiveorxythm@gmail.com>
2025-08-05 13:04:42 +0000

    ONO logo added

--- ono/src/components/HomeHeader.jsx
+++ ono/src/components/HomeHeader.jsx
@@ -2,12 +2,23 @@ import HamburgerIcon from '@/assets/HamburgerHeader.svg'
 import WalletIcon from '@/assets/WalletHeader.svg'
 import BellIcon from '@/assets/BellHeader.svg'
 import { NovuInbox } from './ui/inbox/NovuInbox'
+import ONOLogo from '@/assets/ONOLogo.avif'
+import { Navigate, useNavigate } from 'react-router-dom'
+import { useFrappeAuth } from 'frappe-react-sdk'
 
 const HomeHeader = () => {
+  const navigate = useNavigate()
   return (
     <div className="h-16 select-none w-full p-4 border-b flex justify-between items-center gap-4 border-[#8D8D8D80]/50 max-w-md mx-auto bg-white">
       <div className="flex items-center gap-2.5">
-        <span className="text-[#E26F64] text-lg">ONO</span>
+        <span
+          className="cursor-pointer"
+          onClick={() => {
+            navigate('/')
+          }}
+        >
+          <img src={ONOLogo} width={85} height={40} alt="" />
+        </span>
       </div>
 
       <div className="flex gap-1 items-center">
